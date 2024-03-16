df9546426933a7964b93af2035c25ede5852ba3d
Marvelous Obiji <marvelousobiji@gmail.com>
2024-03-16 13:57:59 +0100

    dart assesment

--- functions.dart
+++ functions.dart
@@ -1,2 +1,18 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+// ## Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+
+void main() {
+  double num1 = 10.0;
+  double num2 = 5.0;
+
+  print("Sum: ${sum(num1, num2)}");
+  print("Difference: ${difference(num1, num2)}");
+}
+
+double sum(double a, double b) {
+  return a + b;
+}
+
+double difference(double a, double b) {
+  return a - b;
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,12 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+// ## Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main() {
+  String name = "Marvelous Obiji";
+  int age = 25;
+  String school = "PLP Africa";
+  String hobby = "storytelling";
+
+  print(
+      "Hey there! My name is $name. I'm feeling like I'm forever $age, currently exploring learning  opportunities at $school, and whenever I'm not busy, you can find me lost in the world of $hobby!");
+}
